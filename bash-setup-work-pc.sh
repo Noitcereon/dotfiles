f1085a0a9a744810e8df295be9015ae9a71e2f99
Thomas 'Noitcereon' Andersen <thomas.andersen2@dxc.com>
2025-03-19 09:20:30 +0100

    Add bash-setup-work-pc

---
+++ bash-setup-work-pc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Custom config (Noitcereon)
+
+# Env variables for trying out PgAdmin4
+export PGADMIN_USER="noit"
+export PGADMIN_PASS="admin"
+
+INTELLIJ="/home/tandersen25/Applications/IntelliJ/idea-IU-243.22562.218/bin/"
+PATH="${INTELLIJ}:${PATH}"
+
+# NEXUS_INI
+source "/home/tandersen25/EE-config/set-EE-env-variables.sh"
+
+# END Custom config
+
